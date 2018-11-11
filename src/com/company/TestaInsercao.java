7fcbf252bf20290e1db6d7fbdf00f58ad901e014
Christiano Piccinin <christiano.piccinin@gmail.com>
2018-11-10 22:47:43 -0200

    update project

--- src/com/company/TestaInsercao.java
+++ src/com/company/TestaInsercao.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -8,8 +9,21 @@ public class TestaInsercao {
     public static void main(String[] args) throws SQLException {
         Connection connection = Database.getConnection();
 
-        Statement stantement = connection.createStatement();
+        Statement statement = connection.createStatement();
 
+        boolean resultado = statement.execute("insert into Produto (nome,descricao) values('Notebook','Mac')",
+                Statement.RETURN_GENERATED_KEYS);
+        System.out.println(resultado);
+
+        ResultSet  resultSet = statement.getGeneratedKeys();
+        while (resultSet.next()){
+            String id = resultSet.getString("id");
+            System.out.println("Id gerado " + id );
+
+
+        }
+
+        statement.close();
         connection.close();
     }
 }
---
+++ src/com/company/TestaRemocao.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.sql.Connection;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaRemocao {
+
+    public static void main(String[] args) throws SQLException {
+        Connection connection = Database.getConnection();
+        Statement stmt = connection.createStatement();
+
+        stmt.execute("delete from Produto where id > 3");
+        int count = stmt.getUpdateCount();
+        System.out.println(count);
+
+        stmt.close();
+        connection.close();
+    }
+}
+
